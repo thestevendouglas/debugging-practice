0283aca7ae80bed36c0ab456e57536fa781bf02a
Steven Antonsen <stevenantonsen@D-108-179-139-40.dhcp4.washington.edu>
2018-04-05 20:01:31 -0700

    updates

--- script.js
+++ script.js
@@ -19,6 +19,8 @@
 var courseList = [];
 // REVIEW: Would this be safer if variable was scoped to window.onload or addACourse functions?
 
+
+
 // Assign handlers to page events.
 window.onload = function(){
   document.querySelector("form#add-course").addEventListener("submit", addACourse)
@@ -30,11 +32,15 @@ window.onload = function(){
   // instead of defining them elsewhere and assigning them here?
 }
 
+
+
 // Triggered on form submit. Creates a new course object and pushes it into courseList array,
 // clears content in form fields, prints courseList objects to the page.
 // BUGFIX: This function isn't working properly- nothing gets output to the list on form submit
 function addACourse(){
+  event.preventDefault()  
   var grade = parseFloat(this.elements["grade"].value);
+  console.log("grade: ", grade)
   /*
     TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
 
@@ -44,6 +50,9 @@ function addACourse(){
     REVIEW: could we make it doubly safe by adding an HTML validation as well?
   */
 
+ 
+  
+  
   // Create the new course with values from the form, push it into array of courses.
   var newCourse = {
     name: this.elements["name"].value,
@@ -51,10 +60,14 @@ function addACourse(){
   }
 
   courseList.push(newCourse)
+  console.log("courseList: ", courseList)
 
   clearFormFields()
   outputList()
 }
+
+
+
 
 // Calculate the average of "grade" attribute for all objects in courseList array
 // and print it in friendly message to page text.
@@ -89,6 +102,16 @@ function clearData(){
 // Prints courseList objects to the page in a readable way.
 function outputList(){
   var list = document.getElementById("course-list");
+  list.innerHTML = ""
+  
+  for(var i=0; i< courseList.length; i++){
+    console.log("name: ", courseList[i].name, "grade: ", courseList[i].grade)
+    
+    var newLi = document.createElement("li")
+    newLi.innerHTML = "name " + courseList[i].name + "grade: " + courseList[i].grade
+    
+    list.appendChild(newLi)
+  }
   /*
     TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
     create an li element that holds the course's name and grade, and append
